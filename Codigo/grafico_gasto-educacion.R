4dfc7b5aaf6e0f50493f0a7c3b60699b8a1a1b51
Alonso Campos <aacampos3@uc.cl>
2021-11-02 11:41:21 -0300

    cambia comentarios en grafico_gasto-educacion

--- Codigo/grafico_gasto-educacion.R
+++ Codigo/grafico_gasto-educacion.R
@@ -4,14 +4,15 @@ library(readr)
 
 
 # cargamos los datos de gastos en educacion en Chile
-gastos <- read_delim("datos/Gastos en educacion/expenditures.csv", na = "..", delim = ",")
+gastos <- read_delim("datos/Gastos en educacion/expenditures.csv",
+                     na = "..", delim = ",")
 
 
 # arreglamos los datos
 # Primero transponemos la tabla y agregamos la columna code_year
-# Segundo Filtramos, quitando los años que no son nulos
+# Segundo filtramos, quitando los datos nulos
 # Especificamos el codigo de la variable de interés
-# Finalmente, agregamos el año
+# Finalmente, agregamos el año como número
 gastos_arreglo <- pivot_longer(
   gastos, cols= `1970 [YR1970]`:`2020 [YR2020]`, names_to = "code_year",
   values_to = "gasto_año"
@@ -28,15 +29,18 @@ gastos_arreglo %>%
   geom_point(col = "turquoise4") + 
   labs(title = "Gasto en educación en Chile entre los años 1993 y 2017",
        subtitle = "Como porcentaje del gasto público total",
-       x = NULL, y = "Gasto en eduación como porcentaje") +
+       x = NULL, y = "Gasto en eduación") +
   scale_y_continuous(labels = scales::percent_format(scale = 1)) +
   scale_x_continuous(breaks = seq(1993, 2017, by =3)) +
   geom_text(aes(label = round(gasto_año, 1)), vjust = - 2, size = 3) +
   theme_minimal()
 
+ggsave("figuras/lineas_gasto_educacion_chile.jpeg", height = 7, width = 10)
+
 # Comentarios:
 # No pude colocar las etiquetas sin que toparan con el gráfico, por lo que hice
-# las lineas de los gráficos un poco más transparantes para que se puediran 
-# notar las etiquetas. Además, en el eje de los años está cada tres años, para
-# que no se sobracargar de información el ejer. Además, omití el nombre de los
-# ejes, porque, en mi opinión, se entiende con el nombre de los ejes.
+# las lineas un poco más transparantes para que se puediran notar las etiquetas.
+# Además, en el eje de los años está graduado cada tres años, para
+# que no se sobracargar de información el eje. Además, omití el nombre del
+# eje x, porque, en mi opinión,con el título se entiende que la variable
+# corresponde a los años
