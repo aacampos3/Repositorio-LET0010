76753934cb38a61488f3123f0efaeb6854779b8e
Alonso Campos <aacampos3@uc.cl>
2021-11-08 12:22:36 -0300

    agrega grafico_explicacion.md y html

--- Codigo/grafico_gasto-educacion.R
+++ Codigo/grafico_gasto-educacion.R
@@ -28,15 +28,37 @@ gastos_arreglo %>%
   ggplot(aes(year, gasto_año)) +
   geom_line(col = "turquoise3", lwd = 1.1, alpha = 0.7) +
   geom_point(col = "turquoise4") + 
+  labs(title = "Figura 1. Gasto en educación en Chile entre los años 1993 y 2017",
+       subtitle = "Como porcentaje del gasto público total",
+       x = NULL, y = "Gasto público en eduación",
+       caption = "Elaboración propia a partir de datos de https://datatopics.worldbank.org/education/") +
+  scale_y_continuous(labels = scales::percent_format(scale = 1),
+                     limits = c(10, 22)) +
+  scale_x_continuous(breaks = seq(1993, 2017, by =3)) +
+  geom_vline(xintercept = 2006, color = "gray80",
+             linetype = "dashed", lwd = 0.75) +
+  geom_text(aes(label = round(gasto_año, 1)), vjust = - 2, size = 3) +
+  theme_minimal() +
+  geom_label(aes(x = 2003.5, y = 21, label = "2006: Revolución Pingüina"),
+             color = "gray51", size = 3)
+
+
+ggsave("figuras/lineas_gasto-educacion_chile.jpeg", height = 7, width = 10)
+
+# Grafico alternativo
+gastos_arreglo %>% 
+  ggplot(aes(year, gasto_año)) +
+  geom_line(col = "turquoise3", lwd = 1.1, alpha = 0.7) +
+  geom_point(col = "turquoise4") + 
   labs(title = "Gasto en educación en Chile entre los años 1993 y 2017",
        subtitle = "Como porcentaje del gasto público total",
        x = NULL, y = "Gasto público en eduación") +
-  scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+  scale_y_continuous(labels = scales::percent_format(scale = 1), limits = c(0, 22)) +
   scale_x_continuous(breaks = seq(1993, 2017, by =3)) +
   geom_text(aes(label = round(gasto_año, 1)), vjust = - 2, size = 3) +
   theme_minimal()
 
-ggsave("figuras/lineas_gasto-educacion_chile.jpeg", height = 7, width = 10)
+ggsave("figuras/lineas_gasto-educacion_chile_alternativo.jpeg", height = 7, width = 10)
 
 # Comentarios:
 # No pude colocar las etiquetas sin que toparan con el gráfico, por lo que hice
